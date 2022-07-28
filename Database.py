c50465201064363023971bbcdf86b244b86a34a8
David Niño-Torres <62343519+jdninot@users.noreply.github.com>
2022-07-27 20:35:03 -0500

    Add files via upload

---
+++ Database.py
@@ -0,0 +1,8 @@
+# Databases
+# 
+# http://biomodelos.humboldt.org.co/
+# http://geonetwork.humboldt.org.co/geonetwork/srv/spa/catalog.search#/home
+# http://i2d.humboldt.org.co/ceiba/
+# http://bdrsnc.sgc.gov.co/paginas1/catalogo/index.php
+# https://www.colombiaenmapas.gov.co/
+# https://estadisticas.cepal.org/cepalstat/web_cepalstat/estadisticasindicadores.asp
